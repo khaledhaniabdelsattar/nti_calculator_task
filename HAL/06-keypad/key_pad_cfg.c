db6367b63f972b5d0199e07e22303ad97dd68c32
khaledhaniabdelsattar <136917641+khaledhaniabdelsattar@users.noreply.github.com>
2023-06-19 20:01:53 +0300

    Add files via upload

---
+++ HAL/06-keypad/key_pad_cfg.c
@@ -0,0 +1,19 @@
+/*
+ * key_pad_cfg.c
+ *
+ *  Created on: Jun 30, 2022
+ *      Author: Khaled
+ */
+
+#include "key_pad.h"
+
+u8 key_pad_buttons[4][4]=
+
+{
+  {'1','2','3','+'},
+  {'4','5','6','-'},
+  {'7','8','9','/'},
+  {'*','0','#','='}
+
+
+};
